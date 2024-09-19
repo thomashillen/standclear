a567138ba35e9801a4aee7d21ad8f8903556df0b
thomashillen <47916202+thomashillen@users.noreply.github.com>
2024-09-19 15:56:10 -0400

    updated interface, seperated subway lines

--- components/SubwayMap.tsx
+++ components/SubwayMap.tsx
@@ -1,73 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import SubwayLine from "./SubwayLine";
 
-// Subway stop data for Line 1, 2, and 3
+// Expanded subway stop data for all NYC subway lines
 const subwayStops = {
   "1": ["South Ferry", "Chambers St", "34th St - Penn Station", "Times Sq - 42nd St", "125th St"],
   "2": ["Flatbush Av", "Atlantic Av - Barclays", "34th St - Penn Station", "Times Sq - 42nd St", "125th St"],
   "3": ["New Lots Av", "Atlantic Av - Barclays", "34th St - Penn Station", "Times Sq - 42nd St", "148th St"],
+  "4": ["Crown Heights", "Atlantic Av - Barclays", "14th St - Union Sq", "Grand Central - 42nd St", "125th St"],
+  "5": ["Flatbush Av", "Atlantic Av - Barclays", "14th St - Union Sq", "Grand Central - 42nd St", "125th St"],
+  "6": ["Brooklyn Bridge", "14th St - Union Sq", "Grand Central - 42nd St", "125th St", "Pelham Bay Park"],
+  "7": ["34th St - Hudson Yards", "Times Sq - 42nd St", "Grand Central - 42nd St", "Queensboro Plaza", "Flushing - Main St"],
+  "A": ["Far Rockaway", "Broadway Junction", "14th St", "59th St - Columbus Circle", "Inwood - 207th St"],
+  "C": ["Euclid Av", "Broadway Junction", "14th St", "59th St - Columbus Circle", "168th St"],
+  "E": ["World Trade Center", "14th St", "34th St - Penn Station", "Jackson Heights - Roosevelt Av", "Jamaica Center"],
+  "B": ["Brighton Beach", "Atlantic Av - Barclays", "34th St - Herald Sq", "59th St - Columbus Circle", "145th St"],
+  "D": ["Coney Island", "Atlantic Av - Barclays", "34th St - Herald Sq", "59th St - Columbus Circle", "161st St - Yankee Stadium"],
+  "F": ["Coney Island", "Church Av", "14th St", "34th St - Herald Sq", "Jamaica - 179th St"],
+  "M": ["Middle Village", "Myrtle - Wyckoff Avs", "14th St", "34th St - Herald Sq", "Forest Hills - 71st Av"],
+  "N": ["Coney Island", "Atlantic Av - Barclays", "34th St - Herald Sq", "Times Sq - 42nd St", "Astoria - Ditmars Blvd"],
+  "Q": ["Coney Island", "Atlantic Av - Barclays", "34th St - Herald Sq", "Times Sq - 42nd St", "96th St"],
+  "R": ["Bay Ridge - 95th St", "Atlantic Av - Barclays", "34th St - Herald Sq", "Times Sq - 42nd St", "Forest Hills - 71st Av"],
+  "W": ["Whitehall St", "Atlantic Av - Barclays", "34th St - Herald Sq", "Times Sq - 42nd St", "Astoria - Ditmars Blvd"],
+  "J": ["Broad St", "Broadway Junction", "Marcy Av", "Essex St", "Jamaica Center"],
+  "Z": ["Broad St", "Broadway Junction", "Marcy Av", "Essex St", "Jamaica Center"],
+  "L": ["8th Av", "14th St - Union Sq", "1st Av", "Bedford Av", "Canarsie - Rockaway Pkwy"],
+  "G": ["Court Sq", "Hoyt - Schermerhorn Sts", "Bedford - Nostrand Avs", "Myrtle - Willoughby Avs", "Church Av"],
+  "S": ["Times Sq - 42nd St", "Grand Central - 42nd St"],
 };
 
-const SubwayLine = ({ lineColor, stops }) => {
-  const [positionTop, setPositionTop] = useState(0);
-  const [positionBottom, setPositionBottom] = useState(100);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setPositionTop((prev) => (prev > 100 ? 0 : prev + 1)); // Move right
-      setPositionBottom((prev) => (prev < 0 ? 100 : prev - 1)); // Move left
-    }, 50); // Adjust speed as necessary
-
-    return () => clearInterval(interval);
-  }, []);
-
-  return (
-    <div className="relative h-20 mb-12 mx-10">
-      {/* Subway Line */}
-      <div
-        className={`relative w-full h-2 ${lineColor || "bg-black"} `}
-      />
-
-      {/* Subway Cars Moving Top to Right */}
-      <div
-        className="absolute h-4 w-10 bg-blue-500 top-3 transition-left duration-75"
-        style={{ left: `${positionTop}%` }}
-      />
-
-      {/* Subway Cars Moving Bottom to Left */}
-      <div
-        className="absolute h-4 w-10 bg-red-500 bottom-3 transition-left duration-75"
-        style={{ left: `${positionBottom}%` }}
-      />
-
-      {/* Render Subway Stops */}
-      {stops.map((stop, index) => {
-        const position = (index / (stops.length - 1)) * 100; // Calculate position between 0% and 100%
-        return (
-          <div
-            key={index}
-            className="absolute text-center "
-            style={{ left: `${position}%`}}
-          >
-            {/* Stop Marker */}
-            <div className="w-2.5 h-2.5 bg-black rounded-full mb-1" />
-            {/* Stop Name */}
-            <div className="text-xs">{stop}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
+const lineColors = {
+  "1": "#EE352E", "2": "#EE352E", "3": "#EE352E",
+  "4": "#00933C", "5": "#00933C", "6": "#00933C",
+  "7": "#B933AD",
+  "A": "#0039A6", "C": "#0039A6", "E": "#0039A6",
+  "B": "#FF6319", "D": "#FF6319", "F": "#FF6319", "M": "#FF6319",
+  "N": "#FCCC0A", "Q": "#FCCC0A", "R": "#FCCC0A", "W": "#FCCC0A",
+  "J": "#996633", "Z": "#996633",
+  "L": "#A7A9AC",
+  "G": "#6CBE45",
+  "S": "#808183",
 };
 
-const SubwayMap = () => {
+const SubwayMap: React.FC = () => {
+  const [selectedLine, setSelectedLine] = useState<string | null>(null);
+
+  const handleLineClick = (line: string) => {
+    setSelectedLine(line === selectedLine ? null : line);
+  };
+
   return (
     <div className="p-5">
-      {/* Line 1 */}
-      <SubwayLine lineColor="bg-red-600" stops={subwayStops["1"]} />
-      {/* Line 2 */}
-      <SubwayLine lineColor="bg-red-600" stops={subwayStops["2"]} />
-      {/* Line 3 */}
-      <SubwayLine lineColor="bg-red-600" stops={subwayStops["3"]} />
+      <div className="flex flex-wrap justify-center gap-4 mb-8">
+        {Object.keys(subwayStops).map((line) => (
+          <button
+            key={line}
+            className={`w-12 h-12 rounded-full text-white font-bold ${
+              selectedLine === line ? "ring-4 ring-blue-300" : ""
+            }`}
+            style={{ backgroundColor: lineColors[line as keyof typeof lineColors] }}
+            onClick={() => handleLineClick(line)}
+          >
+            {line}
+          </button>
+        ))}
+      </div>
+      {selectedLine && (
+        <SubwayLine
+          lineColor={lineColors[selectedLine as keyof typeof lineColors]}
+          stops={subwayStops[selectedLine as keyof typeof subwayStops]}
+        />
+      )}
     </div>
   );
 };
